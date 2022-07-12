2dcd630c2d14ba2ec3caccaa3bbe6d20bbcb9ea2
Ana Sobotincic <ana.sobotincic@gmail.com>
2022-07-12 22:00:04 +0200

    prvi commit

---
+++ js/gumb.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+$("#gumb").click(function () {
+$(this).html($(this).html()=="Manje <i class=\"fas fa-chevron-up\"></i>" ? " Više <i class=\"fas fa-chevron-down\"></i>" : "Manje <i class=\"fas fa-chevron-up\"></i>");
+});
+});
